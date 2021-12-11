9123e530e3ec3011d618af9ed81799b4a52aa8ae
getReema <reeroo02@gmail.com>
2021-12-11 20:57:01 +0300

    Use Formik and Yup

--- App.js
+++ App.js
@@ -1,34 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View,Image,ImageBackground, SafeAreaView ,TextInput,Button, TouchableOpacity} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Formik } from "formik";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import * as Yup from "yup";
 
+const validationSchema = Yup.object().shape({
+  username: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <ImageBackground source={require('./assets/background.png')} resizeMode="cover" style={styles.backgroundImage} blurRadius={10} > 
+      <ImageBackground
+        source={require("./assets/background.png")}
+        resizeMode="cover"
+        style={styles.backgroundImage}
+        blurRadius={10}
+      >
+        <View style={styles.logoContainer}>
+          <Image
+            source={require("./assets/logo.png")}
+            style={styles.logoImage}
+          />
+          <Text style={styles.loginHeader}> Login To Your Account </Text>
+        </View>
 
-      <View style={styles.logoContainer}>
-       
-      <Image source={require('./assets/logo.png')} style={styles.logoImage} />
-        <Text style={styles.loginHeader}> Login To Your Account </Text>
-     
-      </View>
-     
-     <TextInput placeholder="Enter Email" style={styles.TextInput}></TextInput>
-     <TextInput placeholder="Enter Password" style={styles.TextInput}></TextInput>
-
-
-<TouchableOpacity style={styles.loginButton}
-           
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={(values) => console.log(values)}
         >
-            <Text style={styles.loginText}> LOG IN </Text>
-        </TouchableOpacity>
+          {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <>
+              <TextInput
+                name="email"
+                placeholder="Enter your Email Address"
+                style={styles.TextInput}
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                value={values.email}
+                keyboardType="email-address"
+              />
+              <TextInput
+                name="password"
+                placeholder="Enter Your Password"
+                style={styles.TextInput}
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+                secureTextEntry
+              />
 
+              <TouchableOpacity
+                onPress={handleSubmit}
+                style={styles.loginButton}
+              >
+                <Text style={styles.loginText}> LOG IN </Text>
+              </TouchableOpacity>
+            </>
+          )}
+        </Formik>
       </ImageBackground>
-     
     </SafeAreaView>
-    
   );
 }
 
@@ -36,52 +79,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  logoContainer:{
+  logoContainer: {
     marginTop: 10,
-    flex:0.6,
-    alignItems:'center',
-    justifyContent:'center',
+    flex: 0.6,
+    alignItems: "center",
+    justifyContent: "center",
   },
   logoImage: {
     height: 150,
-    width:150,
-    alignSelf: 'center',
+    width: 150,
+    alignSelf: "center",
     marginBottom: 20,
- 
   },
-  loginHeader:{
-    fontSize:25,
-    color:'black',
-    fontStyle:'normal',
-
+  loginHeader: {
+    fontSize: 25,
+    color: "black",
+    fontStyle: "normal",
   },
-  backgroundImage:{
-    flex: 1,  
+  backgroundImage: {
+    flex: 1,
   },
-  TextInput:{
-    backgroundColor:'#FFF',
-    height:60,
+  TextInput: {
+    backgroundColor: "#FFF",
+    height: 60,
     width: 380,
-    padding:10,
-    margin:10,
-    borderRadius:20,
-    color:'pink',
-    alignSelf:'center',
-
+    padding: 10,
+    margin: 10,
+    borderRadius: 20,
+    color: "pink",
+    alignSelf: "center",
   },
-  loginButton:{
+  loginButton: {
     borderRadius: 25,
-    backgroundColor:'black',
+    backgroundColor: "black",
     fontSize: 20,
-    width: '50%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf:'center',
+    width: "50%",
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
     padding: 20,
-    margin:20,
+    margin: 20,
   },
-  loginText:{
-    color:'#FFF',
-    fontSize:15
-  }
+  loginText: {
+    color: "#FFF",
+    fontSize: 15,
+  },
 });
