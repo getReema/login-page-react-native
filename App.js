18805051bd102384fd1b25ad53328c1f8263f085
getReema <reeroo02@gmail.com>
2021-12-11 20:40:14 +0300

    Add Login Fields and Button

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View,Image,ImageBackground } from 'react-native';
+import { StyleSheet, Text, View,Image,ImageBackground, SafeAreaView ,TextInput,Button, TouchableOpacity} from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ImageBackground source={require('./assets/background.png')} resizeMode="cover" style={styles.backgroundImage} > 
+    <SafeAreaView style={styles.container}>
+      <ImageBackground source={require('./assets/background.png')} resizeMode="cover" style={styles.backgroundImage} blurRadius={10} > 
 
       <View style={styles.logoContainer}>
-        <Text> Login to SAIB</Text>
+       
       <Image source={require('./assets/logo.png')} style={styles.logoImage} />
+        <Text style={styles.loginHeader}> Login To Your Account </Text>
+     
       </View>
      
+     <TextInput placeholder="Enter Email" style={styles.TextInput}></TextInput>
+     <TextInput placeholder="Enter Password" style={styles.TextInput}></TextInput>
+
+
+<TouchableOpacity style={styles.loginButton}
+           
+        >
+            <Text style={styles.loginText}> LOG IN </Text>
+        </TouchableOpacity>
+
       </ImageBackground>
-    </View>
+     
+    </SafeAreaView>
     
   );
 }
@@ -23,19 +37,51 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   logoContainer:{
-    marginTop: 80,
-    flex:1,
-    // alignItems:'flex-start',
-    justifyContent:'flex-start'
+    marginTop: 10,
+    flex:0.6,
+    alignItems:'center',
+    justifyContent:'center',
   },
   logoImage: {
     height: 150,
     width:150,
-    alignSelf: 'flex-start',
-    marginBottom: 20
+    alignSelf: 'center',
+    marginBottom: 20,
  
+  },
+  loginHeader:{
+    fontSize:25,
+    color:'black',
+    fontStyle:'normal',
+
   },
   backgroundImage:{
     flex: 1,  
   },
+  TextInput:{
+    backgroundColor:'#FFF',
+    height:60,
+    width: 380,
+    padding:10,
+    margin:10,
+    borderRadius:20,
+    color:'pink',
+    alignSelf:'center',
+
+  },
+  loginButton:{
+    borderRadius: 25,
+    backgroundColor:'black',
+    fontSize: 20,
+    width: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf:'center',
+    padding: 20,
+    margin:20,
+  },
+  loginText:{
+    color:'#FFF',
+    fontSize:15
+  }
 });
